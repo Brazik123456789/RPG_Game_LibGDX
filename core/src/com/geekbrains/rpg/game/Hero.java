e846ed23fbd23c881c1bb4493760386ba47eb347
Азамат Жиеналин <zhienalin.a@yandex.ru>
2020-03-04 02:10:40 +0300

    2 dz

--- core/src/com/geekbrains/rpg/game/Hero.java
+++ core/src/com/geekbrains/rpg/game/Hero.java
@@ -3,72 +3,76 @@ package com.geekbrains.rpg.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 public class Hero {
-    private Texture texture;
+    private Projectile projectile;
+    private TextureRegion texture;
+    private TextureRegion texturePointer;
+    private TextureRegion textureHp;
     private Vector2 position;
+    private Vector2 dst;
+    private Vector2 tmp;
+    private float lifetime;
     private float speed;
-    private float dX = 0;
-    private float dY = 0;
+    private int hp;
+    private int hpMax;
+    private StringBuilder strBuilder;
+    private Apple apple;
 
-
-    public Hero() {
-        this.texture = new Texture("hero.png");
+    public Hero(TextureAtlas atlas) {
+        this.texture = atlas.findRegion("knight");
+        this.texturePointer = atlas.findRegion("pointer");
+        this.textureHp = atlas.findRegion("hp");
         this.position = new Vector2(100, 100);
-        this.speed = 100.0f;
+        this.projectile = new Projectile(atlas);
+        this.dst = new Vector2(position);
+        this.tmp = new Vector2(0, 0);
+        this.speed = 300.0f;
+        this.hpMax = 10;
+        this.hp = 10;
+        this.strBuilder = new StringBuilder();
+        this.apple = new Apple(atlas);
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(texture, position.x - 32, position.y - 32,32,32,64,64,1,1,0,0,0,64,64,false,false);
+        batch.draw(texturePointer, dst.x - 30, dst.y - 30, 30, 30, 60, 60, 0.5f, 0.5f, lifetime * 90.0f);
+        batch.draw(texture, position.x - 30, position.y - 30, 30, 30, 60, 60, 1, 1, 0);
+        batch.draw(textureHp, position.x - 30, position.y + 30, 60 * ((float) hp / hpMax), 12);
+        apple.render(batch);
+        projectile.render(batch);
+
+    }
+
+    public void renderGUI(SpriteBatch batch, BitmapFont font) {
+        strBuilder.setLength(0);
+        strBuilder.append("Class: ").append("Knight").append("\n");
+        strBuilder.append("HP: ").append(hp).append(" / ").append(hpMax).append("\n");
+        font.draw(batch, strBuilder, 10, 710);
     }
 
     public void update(float dt) {
-        if(Gdx.input.isKeyPressed(Input.Keys.A)) {
-            position.x -= speed * dt;
+        projectile.update(dt);
+        lifetime += dt;
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+            dst.set(Gdx.input.getX(), 720.0f - Gdx.input.getY());
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.D)) {
-            position.x += speed * dt;
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.S)) {
-            position.y -= speed * dt;
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.W)) {
-            position.y += speed * dt;
-        }
-
-
-        //  сравниваем координаты мышки с центром картинки
-        if (Gdx.input.justTouched()){
-            if (Gdx.input.getX() != position.x - 32) dX = Gdx.input.getX();
-            if (720.0f - Gdx.input.getY() != position.y - 32) dY = 720.0f - Gdx.input.getY();
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.RIGHT)) {
+            projectile.setup(position.x, position.y, Gdx.input.getX(), 720.0f - Gdx.input.getY());
+//            if (projectile.hit(apple.getPosition())) {
+//                apple.paintApple();
+//            }
         }
 
-        if ((dX != 0) || (dY !=0)) goToMouse(dt);
-
-    }
-
-    //  герой доходит до точки, но продолжает дергаться
-    //  это из-за условий, в конце герой начнет просто менять свои координаты вперед-назад, и, соответственно, будет дергаться.
-    //  Я не знаю, как прописать нормальное условие
-    //  Одним из вариантов было, что если расстояние между координатой героя и координатой мышки меньше скорости speed, то автоматически присвоить координате героя эту координату мышки
-    //  но получается непонятное мне дергание по всему полю
-
-
-    public void goToMouse(float dt){
-
-        if (dX - position.x > 0) {
-                position.x += speed * dt;
-        } else if (dX - position.x < 0) {
-            position.x -= speed * dt;
-        } else dX = 0;
-
-
-        if (dY - position.y > 0) {
-            position.y += speed * dt;
-        } else if (dY - position.y < 0) {
-            position.y -= speed * dt;
-        } else dY = 0;
+        tmp.set(dst).sub(position).nor().scl(speed); // вектор скорости
+        if (position.dst(dst) > speed * dt) {
+            position.mulAdd(tmp, dt);
+        } else {
+            position.set(dst);
+        }
     }
 }
